f3810e07c3515f159ff6b76148220a8f06c9c87a
Aleksandr Fomin <fsasha_1997@mail.ru>
2020-05-16 15:35:04 +0300

    postgres example

--- src/main/java/lab1/App.java
+++ src/main/java/lab1/App.java
@@ -1,14 +1,14 @@
 package lab1;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-//import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-//@EnableJpaRepositories
-@EnableMongoRepositories
+@EnableJpaRepositories
 public class App {
     private static ApplicationContext applicationContext;
 
--- src/main/java/lab1/model/Person.java
+++ src/main/java/lab1/model/Person.java
@@ -1,20 +1,28 @@
 package lab1.model;
 
-import org.springframework.data.annotation.Id;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
 public class Person {
 
     @Id
-    private String id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String name;
     private int age;
+
+    public Person() {
+    }
 
     public Person(String name, int age) {
         this.name = name;
         this.age = age;
     }
 
-    public String getId() {
+    public Long getId() {
         return id;
     }
     public String getName() {
